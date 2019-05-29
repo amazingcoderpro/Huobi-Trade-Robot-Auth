441107ac4e59afd7f8a52cf58a482b492357c17a
Wu Charles <wcadaydayup@163.com>
2019-05-29 19:46:26 +0800

    增加通知接口

--- main.py
+++ main.py
@@ -17,6 +17,7 @@ app = Flask(__name__)
 
 log_config.init_log_config()
 logger = logging.getLogger()
+
 @app.route('/huobi/<key>', methods=['GET'])
 def huobi_verify(key):
 
@@ -49,6 +50,48 @@ def huobi_verify(key):
             else:
                 logger.error("expired 203, key={}".format(key))
                 return ret[1].strftime("%Y-%m-%d %H:%M:%S"), 203
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        logger.info("exception 204, key={}, e={}".format(key, e))
+        return "exception={}".format(e), 204
+
+@app.route('/notify/<key>', methods=['GET'])
+def huobi_notify(key):
+    try:
+        logger.info("huobi notify key={}".format(key))
+        # return "我想对所有人说的话", 200
+        if not key or len(key) < 10:
+            logger.error("invalid key 201, key={}".format(key))
+            return "invalid key, key={}".format(key), 201
+
+        conn = pymysql.connect(
+            host=host,
+            port=port,
+            db=db,
+            user=user,
+            password=password,
+            charset='utf8'
+        )
+        cursor = conn.cursor()
+        cursor.execute("""
+        select account, expire_date from `users` where access_key=%s""", key)
+        ret = cursor.fetchone()
+        if not ret:
+            logger.error("user does not exist 202, key={}".format(key))
+            return "user does not exist.key={}".format(key), 202
+        if ret[1]:
+            if ret[0] == "kanyun":
+                return "阚胖胖, 用的爽不爽？嘿嘿！", 200
+            # else:
+            #     return "", 201
+
+            left = (ret[1]-datetime.datetime.now()).total_seconds()
+            if left <= 0:
+                return u"您的使用期限已经到期, 系统将在24小时内停止工作, 为了避免影响您的收益, 请您尽快联系管理员进行续费！ 联系方式: 15691820861", 200
+            if left/3600/24 < 3:
+                return u"您的使用期限将在 {} 到期, 为了避免影响您的正常使用, 请您尽快联系管理员进行续费！ 联系方式: 15691820861".format(ret[1].strftime("%Y-%m-%d %H:%M:%S")), 200
+
         cursor.close()
         conn.close()
     except Exception as e:
